2cf9c1dd1b229c34f147ff9f0536883aaa0351a5
donidiv <126153831+donidiv@users.noreply.github.com>
2023-12-05 23:02:51 +0200

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
                 <Route path='/register' element={<Register />}/>
                 <Route path='/login' element={<Login />}/>
                 <Route path='/ideas/create' element={<Create />}/>
-                <Route path='/ideas/details' element={<IdeaDetails />}/>
+                <Route path='/ideas/:ideaId/details' element={<IdeaDetails />}/>
                 <Route path='/users/:userId/details' element={<UserProfile />}/>
             </Routes>
 
