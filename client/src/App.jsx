08ad960ab8a77483abdfe433d2d77e31787ac985
donidiv <126153831+donidiv@users.noreply.github.com>
2023-12-05 15:45:14 +0200

    Change directory of the components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,34 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import About from "./components/About";
-import Catalog from "./components/Catalog";
-import Users from "./components/Users";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Create from "./components/Create";
-import IdeaDetails from "./components/IdeaDetails";
-import UserProfile from "./components/UserProfile";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Catalog from "./components/catalog/Catalog";
+import Users from "./components/users-catalog/Users";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+import Create from "./components/create/Create";
+import IdeaDetails from "./components/idea-details/IdeaDetails";
+import UserProfile from "./components/user-profile/UserProfile";
 
 
 
 function App() {
 
-  return (
-    <>
-    <Header />
-    <Home />
-    <About />
-    <Catalog />
-    <Users />
-    <Register />
-    <Login />
-    <Create />
-    <IdeaDetails />
-    <UserProfile />
-
-
-
-
-  
-
-
-    <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Home />
+            <About />
+            <Catalog />
+            <Users />
+            <Register />
+            <Login />
+            <Create />
+            <IdeaDetails />
+            <UserProfile />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
