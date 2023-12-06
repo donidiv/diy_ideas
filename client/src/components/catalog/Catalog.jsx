e3070ce4a1768525f914ad7068d6aa391a584515
donidiv <126153831+donidiv@users.noreply.github.com>
2023-12-06 21:25:20 +0200

    Wip (#17)
    * Add react router library
    
    * Add server
    
    * .
    
    * paths
    
    * ideas.json
    
    * Create game service
    
    * Create idea
    
    * Add get all service method
    
    * Show dynamic game list

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import * as ideaService from '../../services/ideaService';
 
+import CatalogItem from "./CatalogItem";
+
+
 export default function Catalog () {
     const [ideas, setIdeas] = useState([]);
     useEffect(() => {
@@ -25,32 +28,15 @@ export default function Catalog () {
 
             <section className="products section-padding">
                 <div className="container">
-
-                    <div>
+                    {ideas.length === 0 &&
+                    (<div>
                         <h2>There are no ideas yet...</h2>
-                    </div>
-
+                    </div>)
+                    }
                     <div className="row">
-                        <div className="col-lg-4 col-12">
-                            <div className="product-thumb">
-                                <a href="/:ideaId/details">
-                                    <img src="images/product/nature-zen-3Dn1BZZv3m8-unsplash.jpeg" className="img-fluid product-image" alt="" />
-                                </a>
-
-
-
-                                <div className="product-info d-flex">
-                                    <div>
-                                        <h5 className="product-title mb-0">
-                                            <a href="/:ideaId/details" className="product-title-link">Juice Drinks</a>
-                                        </h5>
-
-                                        <p className="product-p"><a href="/:userId/details">owner`s name</a></p>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-                        </div>                       
+                        {ideas.map(idea => (
+                            <CatalogItem key={idea._id} {...idea}/>
+                        ))}
 
                     </div>
                 </div>
