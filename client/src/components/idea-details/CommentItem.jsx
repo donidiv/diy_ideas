c35c0c1e76aa1e4f397ae9e255e51271952ebd04
donidiv <126153831+donidiv@users.noreply.github.com>
2023-12-11 14:31:28 +0200

    Add comments without username

--- client/src/components/idea-details/CommentItem.jsx
+++ client/src/components/idea-details/CommentItem.jsx
@@ -1,21 +1,21 @@
-export default function CommentItem ({
-    username,
+export default function CommentItem({
     text,
+    owner:{email},
 }) {
     return (<div className="slick-testimonial">
-                                <div className="slick-testimonial-caption">
-                                    <p className="lead">{text}</p>
+        <div className="slick-testimonial-caption">
+            <p className="lead">{text}</p>
 
-                                    <div className="slick-testimonial-client d-flex align-items-center mt-4">
-                                        <img src="/images/people/senior-man-wearing-white-face-mask-covid-19-campaign-with-design-space.jpeg" className="img-fluid custom-circle-image me-3" alt="" />
+            <div className="slick-testimonial-client d-flex align-items-center mt-4">
+                <img src="/images/people/senior-man-wearing-white-face-mask-covid-19-campaign-with-design-space.jpeg" className="img-fluid custom-circle-image me-3" alt="" />
 
-                                        <span><strong className="text-muted"><a href="/:userId/details">{username}</a></strong></span>
-                                    </div>
-                                </div>
-                                
-                            </div>
+                <span><strong className="text-muted"><a href="/:userId/details">{email}</a></strong></span>
+            </div>
+        </div>
+
+    </div>
     );
-   
 
-                            
+
+
 }
