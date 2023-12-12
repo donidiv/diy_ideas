db98ecd54b70f3e78aa5441b215773e8ab529b1a
donidiv <126153831+donidiv@users.noreply.github.com>
2023-12-12 13:34:50 +0200

    Persisted state

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,4 +1,10 @@
 import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Path from "../paths";
+
+import * as authService from '../services/authService';
+import usePersistedState from "../hooks/usePersistedState";
 
 const AuthContext = createContext();
 
@@ -6,10 +12,50 @@ AuthContext.displayName = 'AuthContext';
 
 export const AuthProvider = ({
     children,
-    value,
 }) => {
+    const navigate = useNavigate();
+    const [auth, setAuth] = usePersistedState('auth', {});
+
+    const loginSubmitHandler = async (values) => {
+        const result = await authService.login(values.email, values.password);
+        console.log(result);
+
+        setAuth(result);
+
+        localStorage.setItem('accessToken', result.accessToken);
+
+        navigate(Path.Home);
+    };
+    const registerSubmitHandler = async (values) => {
+        console.log(values);
+        const result = await authService.register(values.email, values.password, values.username);
+
+        setAuth(result);
+
+        localStorage.setItem('accessToken', result.accessToken);
+
+
+        navigate(Path.Home);
+    };
+
+
+    const logoutHandler = () => {
+        setAuth({});
+        // navigate(Path.Home);
+        localStorage.removeItem('accessToken');
+    };
+
+    const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        username: auth.username || auth.email,
+        email: auth.email,
+        isAuthenticated: !!auth.accessToken,
+    };
+
     return (
-        <AuthContext.Provider value={value}>
+        <AuthContext.Provider value={values}>
             {children}
         </AuthContext.Provider>
     );
